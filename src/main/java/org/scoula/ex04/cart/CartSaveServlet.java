85d23636d7ac1f5a0aeddac7455bb850a9e02890
ryeon-xx <rlarkgus3415@gmail.com>
2026-07-04 02:38:41 +0900

    07 - 03.2 서블릿 고급 - 세션, 쿠키 (세션을 이용한 장바구니 실습)

---
+++ src/main/java/org/scoula/ex04/cart/CartSaveServlet.java
@@ -0,0 +1,40 @@
+package org.scoula.ex04.cart;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+@WebServlet("/cart/cart_save")
+public class CartSaveServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=UTF-8");
+
+        // 입력 파라미터 얻기
+        String product = req.getParameter("product");
+
+        PrintWriter out = resp.getWriter();
+
+        // 세션 객체 얻기
+        HttpSession session = req.getSession();
+        ArrayList<String> list = (ArrayList<String>) session.getAttribute("product");
+
+        if(list == null) {
+            list = new ArrayList<String>();
+            session.setAttribute("product", list);
+        }
+        list.add(product);
+
+        out.print("<html><body>");
+        out.print("Product 추가 !! <br>");
+        out.println("<a href='session_product.jsp'>상품 선택 페이지</a><br>");
+        out.println("<a href='cart_view'>장바구니 보기</a>");
+        out.print("</body></html>");
+    }
+}
---
+++ src/main/java/org/scoula/ex04/cart/CartViewServlet.java
@@ -0,0 +1,37 @@
+package org.scoula.ex04.cart;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+@WebServlet("/cart/cart_view")
+public class CartViewServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=UTF-8");
+
+        PrintWriter out = resp.getWriter();
+        out.println("<html><body>");
+        out.println("장바구니 리스트 <br>");
+
+        // 세션 객체 얻기
+        HttpSession session = req.getSession(false);
+        session.setMaxInactiveInterval(20);  // 20초
+
+        if(session != null) {
+            ArrayList<String> list = (ArrayList<String>) session.getAttribute("product");
+            out.println("상품 : " + list + "<br>");
+        } else {
+            out.println("세션 없음" + "<br>");
+        }
+        out.println("<a href='session_product.jsp'>상품 선택 페이지</a>");
+        out.println("<a href='cart_delete'>장바구니 비우기</a>");
+        out.println("</body></html>");
+    }
+}
