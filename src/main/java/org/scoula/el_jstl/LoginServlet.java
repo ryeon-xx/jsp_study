fea2360735e012a57c1d7048557b8eea427e4daa
ryeon-xx <rlarkgus3415@gmail.com>
2026-07-04 16:27:33 +0900

    07 - @WebServlet 중복 맵핑명으로 인한 주석 처리

--- src/main/java/org/scoula/el_jstl/LoginServlet.java
+++ src/main/java/org/scoula/el_jstl/LoginServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/login")
+//@WebServlet("/login")  // 중복으로 주석 처리 (원활한 서버 실행을 위해)
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
