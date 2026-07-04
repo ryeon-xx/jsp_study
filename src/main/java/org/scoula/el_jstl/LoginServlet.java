964d0073e4c8acbf09f2996c003f8a93f0780a35
ryeon-xx <rlarkgus3415@gmail.com>
2026-07-04 14:07:18 +0900

    07 - 04.2 EL, JSTL (로그인 EL 실습)

---
+++ src/main/java/org/scoula/el_jstl/LoginServlet.java
@@ -0,0 +1,24 @@
+package org.scoula.el_jstl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userid = req.getParameter("userid");
+        String passwd = req.getParameter("passwd");
+
+        resp.setContentType("text/html; charset=UTF-8");
+
+        req.setAttribute("userid", userid);
+        req.setAttribute("passwd", passwd);
+
+        req.getRequestDispatcher("login.jsp").forward(req, resp);
+    }
+}
